6c14858eb599ed857d10decc323212350f9f8e44
ILoveDuc3000 <nguyenduc04121999@gmail.com>
2022-07-15 13:52:53 +0700

    update Screen ScreenLogin

--- src/Screen/Sign_in_up/index.js
+++ src/Screen/Sign_in_up/index.js
@@ -1,40 +1,58 @@
 import React from 'react';
 import { styles } from "./styles"
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
   View,
   ImageBackground,
   TextInput,
   TouchableOpacity,
+  SafeAreaView,
 } from 'react-native';
 
 const image = { uri: "https://traderclass.vn/public/upload/images/course/thumb/94732711cf31016f5820.jpg" };
 const App = (props) => {
 
   return (
-    <View>
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}></ImageBackground>
-      <Text>Sign</Text>
-      <TouchableOpacity style={[styles.touchLogin]}  onPress={() => props.navigation.navigate('StarScreen')}>
-        <Text style={[styles.textTouch]}>
-          back
-        </Text>
-      </TouchableOpacity>
-      <TextInput
-        style={styles.input}
-        placeholder="User..."
-        placeholderTextColor={'#FF8600'}>
-      </TextInput>
-      <TextInput
-        style={styles.input}
-        placeholder="pasWord"
-        placeholderTextColor={'#FF8600'}>
-      </TextInput>
-    </View>
+    <SafeAreaView style={styles.safeview}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <View style={styles.viewBot}>
+          <View style={styles.tetxx}>
+            <Text style={styles.Text1dong}>Wellcome back</Text>
+            <Text style={styles.Text2dong}>Login to your account</Text>
+          </View>
+          <TextInput
+            style={styles.input}
+            placeholder="Gmail..."
+            placeholderTextColor={'#FF8600'}>
+          </TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="PassWord"
+            placeholderTextColor={'#FF8600'}>
+          </TextInput>
+          <TouchableOpacity style={styles.SignIn} onPress={() => props.navigation.navigate('Loign')}>
+            <Text style={styles.TextTouchableOpacity}>Sign In</Text>
+          </TouchableOpacity>
+          <View style={styles.haidongcuoi}>
+            <TouchableOpacity>
+              <Text style={[styles.textTouch]}>
+                Forgot yout Password
+              </Text>
+            </TouchableOpacity>
+            <View style={styles.viewTouchSignup}>
+              <Text style={styles.textDA}>
+                Don't have an account ? 
+              </Text>
+              <TouchableOpacity onPress={() => props.navigation.navigate('StarScreen')}>
+                <Text style={styles.textSignup}>
+                  Sign up now !
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
+    </SafeAreaView >
   );
 };
 export default App;
--- src/Screen/Sign_in_up/styles.js
+++ src/Screen/Sign_in_up/styles.js
@@ -1,17 +1,78 @@
 import { StyleSheet } from "react-native";
+
 export const styles = StyleSheet.create({
-    input: {
-      borderWidth: 1,
-      width: '100%',
-      paddingVertical: 10,
-      borderRadius: 5,
-      paddingHorizontal: 25,
-      marginVertical: 6,
-      marginBottom: 12
-  
-    },
-    image: {
-      flex: 1,
-      justifyContent: "center"
-    },
-  });
+  safeview: {
+    flex: 1,
+  },
+  viewBot: {
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    backgroundColor: '#171921',
+    padding: 20
+  },
+  tetxx: {
+    marginBottom: 50,
+  },
+  Text1dong: {
+    textAlign: 'center',
+    justifyContent: "center",
+    color: "#FF8600",
+    fontSize: 30,
+    fontWeight: '600',
+  },
+  Text2dong: {
+    textAlign: 'center',
+    justifyContent: "center",
+    color: "#FF8600",
+    fontSize: 15,
+    fontWeight: '600',
+  },
+  input: {
+    borderWidth: 1,
+    width: '100%',
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    marginVertical: 6,
+    marginBottom: 12,
+    borderRadius: 25,
+    borderColor: "#FF8600",
+    fontWeight: '500',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  SignIn: {
+    backgroundColor: "#FF8600",
+    marginBottom: 15,
+    borderRadius: 30,
+    padding: 15,
+    marginTop: 35
+  },
+  TextTouchableOpacity: {
+    textAlign: "center",
+    fontSize: 18,
+    marginLeft: 10,
+    fontWeight: '500',
+  },
+  textTouch: {
+    color: '#FFFFFF',
+    fontSize: 15,
+    lineHeight: 15,
+    textAlign: 'center'
+  },
+  viewTouchSignup: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 20,
+  },
+  textDA: {
+    color: '#FFFFFF'
+  },
+  textSignup: {
+    color: '#FF8600'
+  },
+  haidongcuoi: {
+    marginTop: 50
+  },
+});
